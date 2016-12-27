49cd3cadc72955d7441993648785d0216c886622
AndreyMatvienko <nephertum@gmail.com>
2016-12-27 13:21:57 +0200

    Added loging

--- server/app.js
+++ server/app.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('Hello!');
+    console.log('Home page');
+    res.send('Hello 123!');
 });
 
 app.listen(3000, function () {
